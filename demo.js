9777fedc99719fe60322e7fa080da4a22505c5f4
Luis Rivas <70450227+LuisRG98@users.noreply.github.com>
2022-08-27 14:42:53 -0400

    Modificando el archivo demo.js

--- demo.js
+++ demo.js
@@ -4,3 +4,5 @@ console.log("Hola clase dia 6");
 console.log("Diplomado Fullstack");
 console.log("Hola clase dia 6 - 25/08/2022");
 console.log("Diplomado Fullstack");
+console.log("Buenos noches clase dia 6 - 25/08/2022");
+console.log("Diplomado Fullstack");
